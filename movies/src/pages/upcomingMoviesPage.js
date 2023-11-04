ef2e4fc8a01d50879df034c795871c218639cfbd
Yiwei Liu <2814824250@qq.com>
2023-11-04 02:50:35 +0000

    Fix upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -2,18 +2,13 @@ import React from "react";
 import { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
 
 const UpcomingMoviesPage = (props) => {
+
   const [movies, setMovies] = useState([]);
   const favorites = movies.filter((m) => m.favorite);
   localStorage.setItem("favorites", JSON.stringify(favorites));
-
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
 
   useEffect(() => {
     getUpcomingMovies().then(movies => {
@@ -21,7 +16,16 @@ const UpcomingMoviesPage = (props) => {
     });
   }, []);
 
-  return <PageTemplate title="Upcoming Movies" movies={movies} selectFavorite={addToFavorites} />;
+  return (
+    <PageTemplate 
+      title="Upcoming Movies" 
+      movies={movies} 
+      // selectFavorite={addToFavorites}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
+    />
+  );
 };
 
 export default UpcomingMoviesPage;
